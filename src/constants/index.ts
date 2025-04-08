8933c65ab4771e39cfa65f08ca077fe6f2a8a456
SH J <thdghk5429@gmail.com>
2025-04-08 17:18:09 +0900

    0408 JSH 새 페이지 추가

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,8 +2,10 @@
 export const ROOT_PATH = '/';
 export const AUTH_PATH = 'auth';
 export const MAIN_PATH = 'main';
+export const MAP_PATH = 'map';
 
 // variable: 절대 path 상수 //
 export const ROOT_ABSOLUTE_PATH = ROOT_PATH;
 export const AUTH_ABSOLUTE_PATH = `${ROOT_PATH}${AUTH_PATH}`;
 export const MAIN_ABSOLUTE_PATH = `${ROOT_PATH}${MAIN_PATH}`;
+export const MAP_ABSOLUTE_PATH = `${ROOT_PATH}${MAP_PATH}`;
