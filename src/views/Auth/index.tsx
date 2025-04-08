922dbe9a952f72cb01f8f51d7c20dbd39e8b1f69
SH J <thdghk5429@gmail.com>
2025-04-08 17:46:30 +0900

    0408 JSH 페이지 추가2

--- src/views/Auth/index.tsx
+++ src/views/Auth/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import SignIn from './SignIn'
+import SignUp from './SignUp'
 
 export default function Auth() {
   return (
     <div id='auth-wrapper'>
       <div className='auth-box'>
-        <div></div>
+        <SignIn />
+        <SignUp />
       </div>
     </div>
   )
