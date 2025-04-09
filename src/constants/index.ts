2954cfa89d1b208132cc575090dc79d5df0475ee
SH J <thdghk5429@gmail.com>
2025-04-10 08:58:49 +0900

    0410 JSH first

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,6 +3,13 @@ export const ROOT_PATH = '/';
 export const AUTH_PATH = 'auth';
 export const MAIN_PATH = 'main';
 export const MAP_PATH = 'map';
+export const BOARD_PATH = 'board';
+export const BOARD_WRITE_PATH = 'write';
+export const BOARD_VIEW_PATH = ':boardNumber';
+export const BOARD_UPDATE_PATH = 'update';
+export const MY_PAGE_PATH = 'my-page';
+export const MY_PAGE_MAIN_PATH = 'my-main';
+export const MY_PAGE_USER_INFO_PATH = 'user-info';
 export const OTHERS_PATH = '*';
 
 // variable: 절대 path 상수 //
@@ -10,3 +17,10 @@ export const ROOT_ABSOLUTE_PATH = ROOT_PATH;
 export const AUTH_ABSOLUTE_PATH = `${ROOT_PATH}${AUTH_PATH}`;
 export const MAIN_ABSOLUTE_PATH = `${ROOT_PATH}${MAIN_PATH}`;
 export const MAP_ABSOLUTE_PATH = `${ROOT_PATH}${MAP_PATH}`;
+export const BOARD_ABSOLUTE_PATH = `${ROOT_PATH}${BOARD_PATH}`;
+export const BOARD_WRITE_ABSOLUTE_PATH = `${ROOT_PATH}${BOARD_PATH}/${BOARD_WRITE_PATH}`;
+export const BOARD_VIEW_ABSOLUTE_PATH = (boardNumber: number | string) => `${ROOT_PATH}${BOARD_PATH}/${boardNumber}`;
+export const BOARD_UPDATE_ABSOLUTE_PATH = (boardNumber: number | string) => `${ROOT_PATH}${BOARD_PATH}/${boardNumber}/${BOARD_UPDATE_PATH}`;
+export const MY_PAGE_ABSOLUTE_PATH = `${ROOT_PATH}${MY_PAGE_PATH}`;
+export const MY_PAGE_MAIN_ABSOLUTE_PATH = `${ROOT_PATH}${MY_PAGE_PATH}/${MY_PAGE_MAIN_PATH}`;
+export const MY_PAGE_USER_INFO_ABSOLUTE_PATH = `${ROOT_PATH}${MY_PAGE_PATH}/${MY_PAGE_USER_INFO_PATH}`;
---
+++ src/views/Board/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// 전체 게시글 보기. 게시글 메인. //
+export default function BoardMain() {
+  return (
+    <div>Board</div>
+  )
+}
