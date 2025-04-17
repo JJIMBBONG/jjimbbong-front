a90f63e725fd12993e87a72f2d65806f83cb90f8
yongu09 <https://github.com/yongu09>
2025-04-17 17:19:16 +0900

    board changes

--- src/apis/dto/response/board/get-comment.response.dto.ts
+++ src/apis/dto/response/board/get-comment.response.dto.ts
@@ -2,7 +2,7 @@ import { Comment } from "src/types/interfaces";
 import ResponseDto from "../response.dto";
 
 
-export default interface GetCommentResonseDto extends ResponseDto {
+export default interface GetCommentResponseDto extends ResponseDto {
   
   comments: Comment[];
 }
