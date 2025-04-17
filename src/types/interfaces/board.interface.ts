63a2acf37ee4909d71b83d46cc820eefa98e1575
yongu09 <https://github.com/yongu09>
2025-04-17 09:20:32 +0900

    no changes

--- src/types/interfaces/board.interface.ts
+++ src/types/interfaces/board.interface.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export default interface Board {
   boardNumber: number;
   boardWriteDate: string;
@@ -9,19 +8,4 @@ export default interface Board {
   boardAddress: string;
   boardViewCount: number;
   boardImage: string | null;
-=======
-export default interface RecommandBoard {
-    boardNumber : number;
-    addressCategory : string;
-    detailCategory : string;
-    writeDate: string;
-    title: string;
-    viewCount : number;
-    score : number;
-    image : string;
-}
-
-export default interface Board {
-    
->>>>>>> b8bb6dd0d0cd8533f023cd63f735d7cc660877ed
 }
